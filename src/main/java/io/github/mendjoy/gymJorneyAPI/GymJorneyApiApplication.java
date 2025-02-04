774ee746f5c0de5dd5b48bbb4e04aa42b959436d
mendjoy <joycemendes.jm79@gmail.com>
2025-02-04 19:59:05 -0300

    setupinicial

---
+++ src/main/java/io/github/mendjoy/gymJorneyAPI/GymJorneyApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.mendjoy.gymJorneyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymJorneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymJorneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mendjoy/gymJorneyAPI/GymJorneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mendjoy.gymJorneyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymJorneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
