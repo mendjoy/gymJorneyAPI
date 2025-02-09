8e11cba414e65a23d0e8d473254de26110740341
mendjoy <joycemendes.jm79@gmail.com>
2025-02-09 20:35:54 -0300

    classe para retornar resposra da api

---
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/dto/ResponseApiDTO.java
@@ -0,0 +1,66 @@
+package io.github.mendjoy.gymJourneyAPI.dto;
+
+import org.springframework.http.HttpStatus;
+
+public class ResponseApiDTO {
+
+    private HttpStatus status;
+    private String message;
+    private boolean success;
+    private Object data;
+
+    public ResponseApiDTO() {
+    }
+
+    public ResponseApiDTO(HttpStatus status, String message, boolean success) {
+        this.status = status;
+        this.message = message;
+        this.success = success;
+    }
+
+    public ResponseApiDTO(HttpStatus status, boolean success, Object data) {
+        this.status = status;
+        this.success = success;
+        this.data = data;
+    }
+
+    public static ResponseApiDTO success(HttpStatus status, Object data) {
+        return new ResponseApiDTO(status, true, data);
+    }
+
+    public static ResponseApiDTO error(HttpStatus status, String message) {
+        return new ResponseApiDTO(status, message, false);
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setError(boolean success) {
+        this.success = success;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
--- src/main/java/io/github/mendjoy/gymJourneyAPI/dto/UserAuthDTO.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/dto/UserAuthDTO.java
@@ -1,28 +1,4 @@
 package io.github.mendjoy.gymJourneyAPI.dto;
 
-public class UserAuthenticationDTO {
-
-    private String email;
-    private String password;
-
-    public UserAuthenticationDTO(String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+public class UserAuthDTO {
 }
