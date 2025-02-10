9b70ace47bc7e813ad9fb3cb064c799dc0538360
mendjoy <joycemendes.jm79@gmail.com>
2025-02-10 17:55:12 -0300

    service para criar e validar token

--- src/main/java/io/github/mendjoy/gymJourneyAPI/service/TokenService.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/service/TokenService.java
@@ -1,4 +1,58 @@
 package io.github.mendjoy.gymJourneyAPI.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import io.github.mendjoy.gymJourneyAPI.entity.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secretKey;
+
+    public String generateToken(User user) {
+
+        try {
+
+            Algorithm algorithm = getAlgorithm();
+            return JWT.create()
+                      .withIssuer("gymJourneyAPI")
+                      .withSubject(user.getUsername())
+                      .withExpiresAt(generateExpirationDate())
+                      .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar Token");
+        }
+    }
+
+    public String validateToken(String token) {
+
+        try {
+
+            Algorithm algorithm = getAlgorithm();
+            return JWT.require(algorithm)
+                      .withIssuer("gymJourneyAPI")
+                      .build()
+                      .verify(token)
+                      .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationDate() {
+        return LocalDateTime.now().plusDays(3).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    private Algorithm getAlgorithm() {
+        return Algorithm.HMAC256(secretKey);
+    }
 }
