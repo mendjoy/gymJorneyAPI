88db516bf709fa4aac40740b755ff0c291321757
mendjoy <joycemendes.jm79@gmail.com>
2025-02-10 18:17:10 -0300

    tratamento de excecoes globais

---
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package io.github.mendjoy.gymJourneyAPI.exception;
+
+import io.github.mendjoy.gymJourneyAPI.dto.ResponseApiDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ResponseApiDTO> handleUserNotFoundException(UsernameNotFoundException exception){
+        ResponseApiDTO responseApi = ResponseApiDTO.error(HttpStatus.NOT_FOUND, exception.getMessage());
+        return new ResponseEntity<>(responseApi, responseApi.getStatus());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ResponseApiDTO> handleBadCredentialsException(BadCredentialsException exception){
+        ResponseApiDTO responseApi = ResponseApiDTO.error(HttpStatus.UNAUTHORIZED, exception.getMessage());
+        return new ResponseEntity<>(responseApi, responseApi.getStatus());
+    }
+}
