cb042569a2c5625cd089cbad63715ebf0dab66bd
mendjoy <joycemendes.jm79@gmail.com>
2025-02-11 21:17:43 -0300

    queri method para encontrar usuario pelo id

--- src/main/java/io/github/mendjoy/gymJourneyAPI/repository/UserRepository.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/repository/UserRepository.java
@@ -3,9 +3,13 @@ package io.github.mendjoy.gymJourneyAPI.repository;
 import io.github.mendjoy.gymJourneyAPI.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     public User findByEmail(String email);
 
     public boolean existsByEmail(String email);
+
+    public Optional<User> findById(Integer id);
 }
