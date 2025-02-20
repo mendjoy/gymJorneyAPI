0839a48b98ba40701e42923b5746053b469e1e3d
mendjoy <joycemendes.jm79@gmail.com>
2025-02-20 20:23:30 -0300

    ajustes

--- src/main/java/io/github/mendjoy/gymJourneyAPI/controller/ExerciseController.java
+++ src/main/java/io/github/mendjoy/gymJourneyAPI/controller/ExerciseController.java
@@ -7,16 +7,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/exercises")
+@RestController
+@RequestMapping("/exercises")
 public class ExerciseController {
 
     @Autowired
     private ExerciseService exerciseService;
 
     @PostMapping
-    public ResponseEntity<ResponseApiDTO> register(ExerciseDTO exerciseDTO){
+    public ResponseEntity<ResponseApiDTO> register(@RequestBody ExerciseDTO exerciseDTO){
         ExerciseDTO newExercise = exerciseService.register(exerciseDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(ResponseApiDTO.success(HttpStatus.CREATED, exerciseDTO));
     }
